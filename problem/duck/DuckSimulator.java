05d500f226307c35bca14ef82377f17c67f9ac5a
Harshit Sharma <brown.hash@outlook.com>
2022-06-14 20:19:37 +0530

    duck simulator implementation with behavor in interface

---
+++ problem/duck/DuckSimulator.java
@@ -0,0 +1,8 @@
+public class DuckSimulator {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.display();
+        mallard.performQuack();
+        mallard.performFly();
+    }
+}
